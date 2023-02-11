c3a32043eec7ce96e3d73d6f91803d8ce46a9ca9
Deepansh Pandey <dpdurg123@gmail.com>
2023-02-11 14:36:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const spinnerwrapperel = document.querySelector('.spinner-wrapper');
 
 window.addEventListener('load',()=> {
-    
+setTimeout(() => {
     spinnerwrapperel.style.opacity = '0';
     setTimeout(() => {
         spinnerwrapperel.style.display = 'none';
     }, 300);
+}, 700);
 
 });
